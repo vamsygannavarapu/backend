a733c1e932f45cad1023f44082bf2e079c363c6d
vamsy2112 <vamsy.2112@gmail.com>
2024-04-27 11:25:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/name", (req, res) => {
+  res.send("Hello Vamsy!");
+});
+
+app.get("/search", (req, res) => {
+  res.send("<h1>you are in search page</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
